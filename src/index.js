e09816dc263702dc0a677701e90fdc8890ea278f
harshita gupta <harshita.g.2k@gmail.com>
2025-07-24 20:11:53 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -9,7 +9,13 @@ dotenv.config({
 
 
 connectDB()
-
+.then(() => {  
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 8000}`);
+  });
+})
+.catch((error) => {
+  console.error("Database connection error:", error);});
 
 
 
