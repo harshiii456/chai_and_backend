83961c1df2479f62446fcb891215ee7897df3b86
harshita gupta <harshita.g.2k@gmail.com>
2025-07-27 12:16:01 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -15,4 +15,12 @@ app.use(express.static('public'));
 app.use(cookieParser());
 
 
-export {app};
+//routes
+
+import userRouter from './routes/user.routes.js';
+
+
+//routes declaration
+app.use('/api/v1/users', userRouter);
+
+export default app;
